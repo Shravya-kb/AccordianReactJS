0665c036ca68b4523e80da91e40654931b52b916
Shravya-kb <shravyakbofficial@gmail.com>
2024-08-03 13:40:41 +0530

    Final code with IsOpen

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 /* eslint-disable react/prop-types */
 const faqs = [
   {
@@ -17,35 +19,38 @@ const faqs = [
 function App() {
   return (
     <div>
-      <AccordianComponent/>
+      <AccordianComponent />
     </div>
   );
 }
-function AccordianComponent(){
-  return(
-    <div className="bg-blue-300 h-screen flex justify-center items-center flex-col gap-4">
-      {
-      faqs.map((faq,i)=>(
-        <Modal title={faq.title} text={faq.text} key={i} index={i}/>
-      ))
-      }
-    </div>
-  );
-}
-
-
-
-function Modal({title,text,index}) {
+function AccordianComponent() {
   return (
-    <div >
-      <div className="bg-white min-h-[100px] max-h-[500px] w-[500px] flex flex-col gap-2 p-4 rounded-lg cursor-pointer shadow-lg shadow-green-500">
-        <div className="flex justify-between">
-          <p>{index +1}</p>
-          <p>{title}</p>
-          <p>-</p>
-        </div>
+    <div className="bg-blue-300 h-screen flex justify-center items-center flex-col gap-4">
+      {faqs.map((faq, i) => (
+        <Modal title={faq.title} text={faq.text} key={i} index={i} />
+      ))}
+    </div>
+  );
+}
 
-        <p>{text}</p>
+function Modal({ title, text, index }) {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleIsOpen() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <div>
+      <div
+        className={` bg-white  w-[500px] flex flex-col gap-2 p-4 rounded-lg cursor-pointer shadow-lg shadow-green-500 text-center font-semibold  ${isOpen ? "border-green-400  border-t-8": null}`}
+        onClick={handleIsOpen}
+      >
+        <div className="flex justify-between px-4 py-2">
+          <p>{index + 1}</p>
+           <p className={isOpen? `text-green-400  `: `text-black` }>{title}</p>
+          {/* // <p>{title}</p> */}
+          <p>{isOpen ? "-" : "+"}</p>
+        </div>
+        {isOpen ? <p className="border-gray-400 border-t pt-4">{text}</p> : null}
       </div>
     </div>
   );
